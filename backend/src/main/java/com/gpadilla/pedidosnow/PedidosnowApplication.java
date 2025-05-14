c990233aa228485760246ff1db3894ce03063f19
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2025-05-14 03:21:50 -0300

    Setup frontend and backend projects

---
+++ backend/src/main/java/com/gpadilla/pedidosnow/PedidosnowApplication.java
@@ -0,0 +1,13 @@
+package com.gpadilla.pedidosnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosnowApplication.class, args);
+	}
+
+}
